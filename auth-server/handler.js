8ae48b77844a905049586deb8f2936868e62c79a
andrasbanki <abankiandras@gmail.com>
2021-07-16 15:51:15 +0200

    Updated auth-server/handler.js and static-site-test/test-auth-server.html

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -108,40 +108,38 @@ module.exports.getAccessToken = async (event) => {
 };
 
 module.exports.getCalendarEvents = async (event) => {
-  
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
     redirect_uris[0]
   );
-
   const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
   oAuth2Client.setCredentials({ access_token });
-
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     calendar.events.list(
       {
         calendarId: calendar_id,
         auth: oAuth2Client,
         timeMin: new Date().toISOString(),
         singleEvents: true,
-        orderBy: "startTime",
+        orderBy: 'startTime',
       },
-      (error, response) => {
-        if (error) {
-          reject(error);
+      (err, res) => {
+        if (err) {
+         return reject(err);
         } else {
-          resolve(response);
+         return resolve(res);
         }
       }
-    )
+    );
+  })
     .then((results) => {
       return {
         statusCode: 200,
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          'Access-Control-Allow-Origin': '*',
         },
-        body: JSON.stringify({ events: results.data.items }),
+        body: JSON.stringify({ events: results.data.items })
       };
     })
     .catch((err) => {
@@ -149,12 +147,9 @@ module.exports.getCalendarEvents = async (event) => {
       return {
         statusCode: 500,
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          'Access-Control-Allow-Origin': '*',
         },
         body: JSON.stringify(err),
       };
     });
-  });
 }
-
-
