806f53b1882b94ceaca91d9212b530bdcaa4d6e2
andrasbanki <abankiandras@gmail.com>
2021-07-26 15:40:11 +0200

    Registered service worker and customized manifest

--- src/index.js
+++ src/index.js
@@ -8,17 +8,12 @@ import * as atatus from 'atatus-spa';
 
 atatus.config('5a66c690c1f149d3959e40169e24e4da').install();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
